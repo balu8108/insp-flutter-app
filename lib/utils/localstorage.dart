1b2d4a01b9bef319b01626763648b6bcb987da47
rhythm-yadav <yrhythm34@gmail.com>
2024-08-13 11:41:07 +0530

    Added shared preference package and modification in local storage

--- lib/utils/localstorage.dart
+++ lib/utils/localstorage.dart
@@ -1,43 +1,55 @@
-import 'package:flutter/foundation.dart'; // For kIsWeb and defaultTargetPlatform
+// import 'package:flutter/foundation.dart'; // For kIsWeb and defaultTargetPlatform
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'dart:html' as html; // For web local storage
+// import 'dart:io' show Platform; // For desktop platforms
+
+// Future<void> storeData(String key, String value) async {
+//   if (kIsWeb) {
+//     // Store data in web's LocalStorage
+//     html.window.localStorage[key] = value;
+//   } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+//       defaultTargetPlatform == TargetPlatform.android) {
+//     // Store data in SharedPreferences for mobile
+//     final sharedPrefs = await SharedPreferences.getInstance();
+//     await sharedPrefs.setString(key, value);
+//   } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+//     // Store data in SharedPreferences for desktop
+//     final sharedPrefs = await SharedPreferences.getInstance();
+//     await sharedPrefs.setString(key, value);
+//   } else {
+//     // Handle other platforms if necessary
+//     print('Unsupported platform for storing data.');
+//   }
+// }
+
+// Future<String?> getData(String key) async {
+//   if (kIsWeb) {
+//     // Retrieve data from web's LocalStorage
+//     return html.window.localStorage[key];
+//   } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+//       defaultTargetPlatform == TargetPlatform.android) {
+//     // Retrieve data from SharedPreferences for mobile
+//     final sharedPrefs = await SharedPreferences.getInstance();
+//     return sharedPrefs.getString(key);
+//   } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+//     // Retrieve data from SharedPreferences for desktop
+//     final sharedPrefs = await SharedPreferences.getInstance();
+//     return sharedPrefs.getString(key);
+//   } else {
+//     // Handle other platforms if necessary
+//     print('Unsupported platform for retrieving data.');
+//     return null;
+//   }
+// }
+
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:html' as html; // For web local storage
-import 'dart:io' show Platform; // For desktop platforms
 
 Future<void> storeData(String key, String value) async {
-  if (kIsWeb) {
-    // Store data in web's LocalStorage
-    html.window.localStorage[key] = value;
-  } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-      defaultTargetPlatform == TargetPlatform.android) {
-    // Store data in SharedPreferences for mobile
-    final sharedPrefs = await SharedPreferences.getInstance();
-    await sharedPrefs.setString(key, value);
-  } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    // Store data in SharedPreferences for desktop
-    final sharedPrefs = await SharedPreferences.getInstance();
-    await sharedPrefs.setString(key, value);
-  } else {
-    // Handle other platforms if necessary
-    print('Unsupported platform for storing data.');
-  }
+  final sharedPrefs = await SharedPreferences.getInstance();
+  await sharedPrefs.setString(key, value);
 }
 
 Future<String?> getData(String key) async {
-  if (kIsWeb) {
-    // Retrieve data from web's LocalStorage
-    return html.window.localStorage[key];
-  } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-      defaultTargetPlatform == TargetPlatform.android) {
-    // Retrieve data from SharedPreferences for mobile
-    final sharedPrefs = await SharedPreferences.getInstance();
-    return sharedPrefs.getString(key);
-  } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    // Retrieve data from SharedPreferences for desktop
-    final sharedPrefs = await SharedPreferences.getInstance();
-    return sharedPrefs.getString(key);
-  } else {
-    // Handle other platforms if necessary
-    print('Unsupported platform for retrieving data.');
-    return null;
-  }
+  final sharedPrefs = await SharedPreferences.getInstance();
+  return sharedPrefs.getString(key);
 }
