644dd3ca381ad86ce3a6e667a6c8650ffb2949a9
Harshit Pandey <harshitpandey@Harshits-MacBook-Air.local>
2024-08-20 10:23:18 +0530

    add teacher condition

--- lib/pages/common/calender/widget/liveclass_calendar.dart
+++ lib/pages/common/calender/widget/liveclass_calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inspflutterfrontend/utils/userDetail/getUserDetail.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class LiveclassCalendar extends StatefulWidget {
@@ -29,98 +30,112 @@ class _LiveclassCalendarState extends State<LiveclassCalendar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0), // Padding around the buttons
-            child: Row(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(
-                        200, 50.0), // Set a fixed size for the button
-                    backgroundColor: const Color.fromRGBO(
-                        60, 141, 188, 1), // Background color
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5), // Border radius
+        backgroundColor: Colors.white,
+        body: FutureBuilder<bool>(
+            future: isTeacherLogin(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(child: Text('Error loading data'));
+              } else {
+                bool isTeacher = snapshot.data ?? false;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(
+                          8.0), // Padding around the buttons
+                      child: Row(
+                        children: [
+                          if (isTeacher)
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(200,
+                                    50.0), // Set a fixed size for the button
+                                backgroundColor: const Color.fromRGBO(
+                                    60, 141, 188, 1), // Background color
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(5), // Border radius
+                                ),
+                              ),
+                              onPressed: () {
+                                // Handle "View Time Table" button press
+                              },
+                              child: const Text(
+                                'Upload Time Table',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(200, 50.0),
+                              backgroundColor:
+                                  const Color.fromRGBO(60, 141, 188, 1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            onPressed: () {
+                              // Handle "Upload Time Table" button press
+                            },
+                            child: const Text(
+                              'View Time Table',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    // Handle "View Time Table" button press
-                  },
-                  child: const Text(
-                    'Upload Time Table',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(200, 50.0),
-                    backgroundColor: const Color.fromRGBO(60, 141, 188, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
+                    const SizedBox(height: 15),
+                    Expanded(
+                      child: ValueListenableBuilder<DateTime>(
+                        valueListenable: _selectedDay,
+                        builder: (context, selectedDay, _) {
+                          return TableCalendar(
+                            firstDay: DateTime.utc(2010, 10, 16),
+                            lastDay: DateTime.utc(2030, 3, 14),
+                            focusedDay: _focusedDay,
+                            calendarFormat: _calendarFormat,
+                            headerStyle: const HeaderStyle(
+                              formatButtonVisible: false,
+                              titleCentered: true,
+                            ),
+                            rowHeight: 60,
+                            availableGestures: AvailableGestures.all,
+                            selectedDayPredicate: (day) {
+                              return isSameDay(selectedDay, day);
+                            },
+                            onDaySelected: (selectedDay, focusedDay) {
+                              if (!isSameDay(_selectedDay.value, selectedDay)) {
+                                _selectedDay.value = selectedDay;
+                                setState(() {
+                                  _focusedDay = focusedDay;
+                                });
+                              }
+                            },
+                            onFormatChanged: (format) {
+                              if (_calendarFormat != format) {
+                                setState(() {
+                                  _calendarFormat = format;
+                                });
+                              }
+                            },
+                            onPageChanged: (focusedDay) {
+                              _focusedDay = focusedDay;
+                            },
+                            onDayLongPressed: (day, focusedDay) {
+                              _selectedDay.value = day;
+                            },
+                          );
+                        },
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    // Handle "Upload Time Table" button press
-                  },
-                  child: const Text(
-                    'View Time Table',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 15),
-          Expanded(
-            child: ValueListenableBuilder<DateTime>(
-              valueListenable: _selectedDay,
-              builder: (context, selectedDay, _) {
-                return TableCalendar(
-                  firstDay: DateTime.utc(2010, 10, 16),
-                  lastDay: DateTime.utc(2030, 3, 14),
-                  focusedDay: _focusedDay,
-                  calendarFormat: _calendarFormat,
-                  headerStyle: const HeaderStyle(
-                    formatButtonVisible: false,
-                    titleCentered: true,
-                  ),
-                  rowHeight: 60,
-                  availableGestures: AvailableGestures.all,
-                  selectedDayPredicate: (day) {
-                    return isSameDay(selectedDay, day);
-                  },
-                  onDaySelected: (selectedDay, focusedDay) {
-                    if (!isSameDay(_selectedDay.value, selectedDay)) {
-                      _selectedDay.value = selectedDay;
-                      setState(() {
-                        _focusedDay = focusedDay;
-                      });
-                    }
-                  },
-                  onFormatChanged: (format) {
-                    if (_calendarFormat != format) {
-                      setState(() {
-                        _calendarFormat = format;
-                      });
-                    }
-                  },
-                  onPageChanged: (focusedDay) {
-                    _focusedDay = focusedDay;
-                  },
-                  onDayLongPressed: (day, focusedDay) {
-                    _selectedDay.value = day;
-                  },
+                  ],
                 );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+              }
+            }));
   }
 }
