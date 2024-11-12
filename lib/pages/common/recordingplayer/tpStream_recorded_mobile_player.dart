f1b8ccf52c2a1a36128b063b3967db797b86c454
balabureau <atluri@bureau.id>
2024-11-12 19:09:21 +0530

    added watermark for mobile video

--- lib/pages/common/recordingplayer/tpStream_recorded_mobile_player.dart
+++ lib/pages/common/recordingplayer/tpStream_recorded_mobile_player.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:insp/utils/userDetail/getUserDetail.dart';
 import 'package:tpstreams_player_sdk/tpstreams_player.dart';
+
+import '../../../apiservices/models/login/login_response_model.dart';
 
 class TPStreamRecordedMobilePlayer extends StatefulWidget {
   const TPStreamRecordedMobilePlayer(
@@ -14,12 +17,47 @@ class TPStreamRecordedMobilePlayer extends StatefulWidget {
 
 class _TPStreamRecordedMobilePlayerState
     extends State<TPStreamRecordedMobilePlayer> {
+  late LoginResponseModelResult userData;
+
+  @override
+  void initState() {
+    super.initState();
+    initUserData();
+  }
+
+  Future<void> initUserData() async {
+    setState(() async {
+      userData = await getUserData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       widget.accessToken.isNotEmpty && widget.accestID.isNotEmpty
-          ? TPStreamPlayer(
-              assetId: widget.accestID, accessToken: widget.accessToken)
+          ? Stack(
+              alignment: Alignment.center,
+              children: [
+                TPStreamPlayer(
+                    assetId: widget.accestID, accessToken: widget.accessToken),
+                Column(
+                  children: [
+                    Text(
+                      userData.email,
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.4),
+                      ),
+                    ),
+                    Text(
+                      userData.mobile,
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.4),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
           : const Text("waiting...")
     ]);
   }
